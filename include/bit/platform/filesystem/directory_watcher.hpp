a04cf3ec01dde416562dec637b91c9aaebede486
Matthew Rodusek <matthew.rodusek@gmail.com>
2017-09-04 15:34:08 -0400

    Added documentation for directory_watcher

--- include/bit/platform/filesystem/directory_watcher.hpp
+++ include/bit/platform/filesystem/directory_watcher.hpp
@@ -10,14 +10,18 @@
 #ifndef BIT_PLATFORM_FILESYSTEM_DIRECTORY_WATCHER_HPP
 #define BIT_PLATFORM_FILESYSTEM_DIRECTORY_WATCHER_HPP
 
-#include <bit/stl/stddef.hpp> // stl::function_t
-#include <bit/stl/delegate.hpp>
+#include <bit/stl/stddef.hpp>   // stl::function_t
+#include <bit/stl/delegate.hpp> // stl::delegate
 
 #include <memory> // std::unique_ptr
 
 namespace bit {
   namespace platform {
 
+    //////////////////////////////////////////////////////////////////////////
+    /// \brief Indicates the type of file action performed on directory
+    ///        notification
+    //////////////////////////////////////////////////////////////////////////
     enum class file_action
     {
       added,    ///< The file was added
@@ -27,8 +31,11 @@ namespace bit {
     };
 
     //////////////////////////////////////////////////////////////////////////
-    /// \brief This class manages watching changes from directories,
+    /// \brief This class manages watching directories for any file or
+    ///        directory change events.
     ///
+    /// \note Registered callbacks are not invoked on the same thread as the
+    ///       thread that creates this watcher
     //////////////////////////////////////////////////////////////////////////
     class directory_watcher
     {
@@ -44,8 +51,11 @@ namespace bit {
       //----------------------------------------------------------------------
     public:
 
+      /// \brief Constructs a directory watcher
       directory_watcher();
 
+      /// \brief Destructs this directory watcher, halting any background
+      ///        threads
       ~directory_watcher();
 
       //----------------------------------------------------------------------
@@ -53,14 +63,38 @@ namespace bit {
       //----------------------------------------------------------------------
     public:
 
+      /// \brief Stops this directory watcher.
+      ///
+      /// Any further attempts to register listeners become no-ops
       void stop();
 
+      //----------------------------------------------------------------------
+
+      /// \brief Registers a listener for a given \p directory
+      ///
+      /// \note This function is not thread-safe. As such, synchronization
+      ///       needs to be done if 'register_listener' is to be called on
+      ///       a different thread, such as the notification thread.
+      ///
+      /// \tparam callback the function pointer to register
+      /// \param directory the directory to view
+      /// \param recursive whether to recursively check subdirectories
       template<stl::function_t<void(stl::string_view,stl::string_view,file_action)> callback>
       void register_listener( stl::string_view directory,
                               bool recursive = true );
 
-      //----------------------------------------------------------------------
-
+      /// \{
+      /// \brief Registers a listener for a given \p directory
+      ///
+      /// \note This function is not thread-safe. As such, synchronization
+      ///       needs to be done if 'register_listener' is to be called on
+      ///       a different thread, such as the notification thread.
+      ///
+      /// \tparam T the type of the underlying instance
+      /// \tparam callback the member function pointer to register
+      /// \param instance the instance to invoke the member function on
+      /// \param directory the directory to view
+      /// \param recursive whether to recursively check subdirectories
       template<typename T, stl::member_function_t<T,void(stl::string_view,stl::string_view,file_action)> callback>
       void register_listener( T* instance,
                               stl::string_view directory,
@@ -71,8 +105,6 @@ namespace bit {
                               stl::string_view directory,
                               bool recursive = true );
 
-      //----------------------------------------------------------------------
-
       template<typename T, stl::member_function_t<const T,void(stl::string_view,stl::string_view,file_action)> callback>
       void register_listener( const T& instance,
                               stl::string_view directory,
@@ -82,6 +114,15 @@ namespace bit {
       void register_listener( T& instance,
                               stl::string_view directory,
                               bool recursive = true );
+      /// \}
+
+      template<typename T, stl::member_function_t<T,void(stl::string_view,stl::string_view,file_action)>>
+      void register_listener( std::nullptr_t, stl::string_view, bool = true ) = delete;
+
+      template<typename T, stl::member_function_t<const T,void(stl::string_view,stl::string_view,file_action)>>
+      void register_listener( std::nullptr_t, stl::string_view, bool = true ) = delete;
+
+      // TODO: Unregister directory watchers
 
       //----------------------------------------------------------------------
       // Private Modifiers
