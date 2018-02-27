d75c3ebb5bea8c9bfd45659d8bb5a63b02f290a3
Matthew Rodusek <matthew.rodusek@gmail.com>
2018-02-27 00:14:56 -0500

    Remove blocking actions from 'this_task_scheduler'
    This was an accidental oversight in one of the previous commits. Waiting
    and blocking on the same scheduler as the currently executing one is
    likely to be cause deadlocks

--- include/bit/platform/threading/this_task_scheduler.hpp
+++ include/bit/platform/threading/this_task_scheduler.hpp
@@ -38,11 +38,6 @@ namespace bit {
       /// \param task the task to post
       static void post_task( task task );
 
-      /// \brief Waits for a given task in this dispatch queue
-      ///
-      /// \param task the task handle to wait on
-      static void wait( task_handle task );
-
       //-----------------------------------------------------------------------
 
       /// \{
@@ -52,109 +47,18 @@ namespace bit {
       /// \param parent the task to designate as parent
       /// \param fn the function to post
       /// \param args the arguments to forward
-      template<typename Fn, typename...Args>
+      template<typename Fn, typename...Args,
+               typename=std::enable_if_t<stl::is_invocable<Fn,Args...>::value>>
       static void post( Fn&& fn, Args&&...args );
-      template<typename Fn, typename...Args>
+      template<typename Fn, typename...Args,
+               typename=std::enable_if_t<stl::is_invocable<Fn,Args...>::value>>
       static void post( const task& parent, Fn&& fn, Args&&...args );
-      /// \}
-
-      //-----------------------------------------------------------------------
-
-      /// \{
-      /// \brief Posts a task in this dispatch serial scheduler, waiting for the result
-      ///
-      /// \param parent the parent task
-      /// \param fn the function to dispatch
-      /// \param args the arguments to forward to the function
-      /// \return the result of the function
-      template<typename Fn, typename...Args>
-      static stl::invoke_result_t<Fn,Args...>
-        post_and_wait( Fn&& fn, Args&&...args );
-      template<typename Fn, typename...Args>
-      static stl::invoke_result_t<Fn,Args...>
-        post_and_wait( const task& parent, Fn&& fn, Args&&...args );
       /// \}
 
     }; // class this_task_scheduler
   } // namespace platform
 } // namespace bit
 
-//=============================================================================
-// Inline Definitions : this_task_scheduler
-//=============================================================================
-
-//-----------------------------------------------------------------------------
-// Posting / Waiting
-//-----------------------------------------------------------------------------
-
-inline void bit::platform::this_task_scheduler::post_task( task task )
-{
-  auto scheduler = task_scheduler::g_active_scheduler;
-
-  assert( scheduler != nullptr );
-
-  scheduler->post_task( std::move(task) );
-}
-
-inline void bit::platform::this_task_scheduler::wait( task_handle task )
-{
-  auto scheduler = task_scheduler::g_active_scheduler;
-
-  assert( scheduler != nullptr );
-
-  scheduler->wait( task );
-}
-
-//-----------------------------------------------------------------------------
-
-template<typename Fn, typename...Args>
-inline void bit::platform::this_task_scheduler::post( Fn&& fn, Args&&...args )
-{
-  auto scheduler = task_scheduler::g_active_scheduler;
-
-  assert( scheduler != nullptr );
-
-  scheduler->post( std::forward<Fn>(fn), std::forward<Args>(args)... );
-}
-
-template<typename Fn, typename...Args>
-inline void bit::platform::this_task_scheduler
-  ::post( const task& parent, Fn&& fn, Args&&...args )
-{
-  auto scheduler = task_scheduler::g_active_scheduler;
-
-  assert( scheduler != nullptr );
-
-  scheduler->post( parent, std::forward<Fn>(fn), std::forward<Args>(args)... );
-}
-
-//-----------------------------------------------------------------------------
-
-
-template<typename Fn, typename...Args>
-inline bit::stl::invoke_result_t<Fn,Args...>
-  bit::platform::this_task_scheduler::post_and_wait( Fn&& fn, Args&&...args )
-{
-  auto scheduler = task_scheduler::g_active_scheduler;
-
-  assert( scheduler != nullptr );
-
-  scheduler->post_and_wait( std::forward<Fn>(fn),
-                            std::forward<Args>(args)... );
-}
-
-template<typename Fn, typename...Args>
-inline bit::stl::invoke_result_t<Fn,Args...>
-  bit::platform::this_task_scheduler
-  ::post_and_wait( const task& parent, Fn&& fn, Args&&...args )
-{
-  auto scheduler = task_scheduler::g_active_scheduler;
-
-  assert( scheduler != nullptr );
-
-  scheduler->post_and_wait( parent,
-                            std::forward<Fn>(fn),
-                            std::forward<Args>(args)... );
-}
+#include "detail/this_task_scheduler.inl"
 
 #endif /* BIT_PLATFORM_THREADING_THIS_TASK_SCHEDULER_HPP */
